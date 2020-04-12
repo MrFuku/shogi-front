212aba6ae31833c3c56ffa6c8f9974ff78a30791
MrFuku <mmafighter_shun@icloud.com>
2020-04-12 17:35:40 +0900

    将棋盤のベース作成

---
+++ app/components/parts/shogi/Board.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="board">
+    <table border="3">
+      <tr v-for="y in size" :key="y">
+        <td class="square" v-for="x in size" :key="x"></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    size: 9
+  }),
+}
+</script>
+
+<style>
+  table {
+    border-collapse: collapse;
+  }
+
+  .square {
+    height: 50px;
+    width: 50px;
+    border-width: 2px;
+    background-color: #F9C270;
+  }
+</style>
+
