18d2c99abebc620155c8387949c2322a35aa64be
MrFuku <mmafighter_shun@icloud.com>
2020-04-12 22:01:48 +0900

    フォーマッタ適用

--- app/components/parts/shogi/Board.vue
+++ app/components/parts/shogi/Board.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="board">
     <table border="3">
-      <tr v-for="y in size" :key="y">
-        <td class="square" v-for="x in size" :key="x"></td>
+      <tr
+        v-for="y in size"
+        :key="y"
+      >
+        <td
+          class="square"
+          v-for="x in size"
+          :key="x"
+        ></td>
       </tr>
     </table>
   </div>
@@ -12,24 +19,24 @@
 export default {
   data: () => ({
     size: 9
-  }),
-}
+  })
+};
 </script>
 
 <style scoped>
-  table {
-    border-collapse: collapse;
-  }
+table {
+  border-collapse: collapse;
+}
 
-  .square {
-    height: 50px;
-    width: 50px;
-    border-width: 2px;
-    background-color: #F9C270;
-  }
+.square {
+  height: 50px;
+  width: 50px;
+  border-width: 2px;
+  background-color: #f9c270;
+}
 
-  .square:hover {
-    background-color: rgb(207, 177, 131);
-  }
+.square:hover {
+  background-color: rgb(207, 177, 131);
+}
 </style>
 
