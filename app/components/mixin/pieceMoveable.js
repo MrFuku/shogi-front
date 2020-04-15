66df5058a2117c256d561b5c6b66f3a27a0826d4
MrFuku <mmafighter_shun@icloud.com>
2020-04-15 22:54:48 +0900

    香車用移動可否判定メソッド定義

--- app/components/mixin/pieceMoveable.js
+++ app/components/mixin/pieceMoveable.js
@@ -1,11 +1,27 @@
 export default {
   methods: {
+    lanceExploration(table, point) {
+      return this.repeat(table, point[0], point[1], [-1, 0]);
+    },
     pawnExploration(table, point) {
       table[point[0] - 1][point[1]] = 1;
       return table;
     },
     getExploration(type) {
-      return this.pawnExploration;
+      if (type === 11) {
+        return this.lanceExploration;
+      } else {
+        return this.pawnExploration;
+      }
+    },
+    repeat(table, y, x, direction) {
+      y += direction[0];
+      x += direction[1];
+      if (this.isTableRange(y, x)) {
+        table[y][x] = 1;
+        this.repeat(table, y, x, direction);
+      }
+      return table;
     },
     isTableRange(y, x) {
       if (y < 0 || x < 0) return false;
