684447928655ca413d27e19f11505f23f16e7b02
MrFuku <mmafighter_shun@icloud.com>
2020-04-16 21:24:34 +0900

    桂馬用移動可否判定メソッド定義

--- app/components/mixin/pieceMoveable.js
+++ app/components/mixin/pieceMoveable.js
@@ -52,6 +52,16 @@ export default {
       );
       return table;
     },
+    knightExploration(table, point) {
+      const directions = [
+        [-2, -1],
+        [-2, 1],
+      ];
+      directions.forEach((dir) =>
+        this.isPutDown(table, point[0] + dir[0], point[1] + dir[1])
+      );
+      return table;
+    },
     lanceExploration(table, point) {
       return this.repeat(table, point[0], point[1], [-1, 0]);
     },
@@ -71,6 +81,8 @@ export default {
           return this.goldExploration;
         case 7:
           return this.silverExploration;
+        case 9:
+          return this.knightExploration;
         case 11:
           return this.lanceExploration;
         default:
